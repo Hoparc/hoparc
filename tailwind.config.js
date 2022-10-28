c25b0fe5fb7de16f535a7eec1b37cb7d3d826c5b
Alvin Quach <alvinwquach@gmail.com>
2022-10-27 18:25:02 -0700

    Create Navbar, MenuLink, and Layout component, added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          150: "#eee",
+        },
+        blue: {
+          350: "#327CDF",
+        },
+        green: {
+          350: "96CA5B",
+        },
+      },
+      height: {
+        "25vh": "25vh",
+        "33vh": "33vh",
+        "50vh": "50vh",
+        "66vh": "66vh",
+        "75vh": "75vh",
+        "90vh": "90vh",
+      },
+      minHeight: {
+        "25vh": "25vh",
+        "33vh": "33vh",
+        "50vh": "50vh",
+        "66vh": "66vh",
+        "75vh": "75vh",
+        "90vh": "90vh",
+      },
+      maxHeight: {
+        "25vh": "25vh",
+        "33vh": "33vh",
+        "50vh": "50vh",
+        "66vh": "66vh",
+        "75vh": "75vh",
+        "90vh": "90vh",
+      },
+    },
   },
   plugins: [],
-}
+};
