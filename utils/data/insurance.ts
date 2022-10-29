d58a377fd4f846c0657681cc7ba67eede36fa0a5
Alvin Quach <alvinwquach@gmail.com>
2022-10-28 18:03:05 -0700

    Create about us page

---
+++ utils/data/insurance.ts
@@ -0,0 +1,52 @@
+export default [
+  {
+    id: 1,
+    image: "/images/about/blueshield.jpeg",
+    alt: "Image of BlueCross BlueShield logo",
+  },
+  {
+    id: 2,
+    image: "/images/about/chambersofcommerce.jpeg",
+    alt: "Image of Chambers of Commerce Group Insurance Plan logo",
+  },
+  {
+    id: 3,
+    image: "/images/about/cowan.jpeg",
+    alt: "Image of Cowan Insurance Group logo",
+  },
+  {
+    id: 4,
+    image: "/images/about/greenshield.jpeg",
+    alt: "Image of Green Shield Canada logo",
+  },
+  {
+    id: 5,
+    image: "/images/about/johnson.jpeg",
+    alt: "Image of Johnson logo",
+  },
+  {
+    id: 6,
+    image: "/images/about/manulife.jpeg",
+    alt: "Image of Manulife logo",
+  },
+  {
+    id: 7,
+    image: "/images/about/sunlife.jpeg",
+    alt: "Image of Sun Life Financial logo",
+  },
+  {
+    id: 8,
+    image: "/images/about/thegreatwestlife.jpeg",
+    alt: "Image of The Great West Life logo",
+  },
+  {
+    id: 9,
+    image: "/images/about/canadalife.jpeg",
+    alt: "Image of Canada Life logo",
+  },
+  {
+    id: 10,
+    image: "/images/about/aon.jpeg",
+    alt: "Image of AON logo",
+  },
+];
