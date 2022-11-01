ce113570c64b515271b11d6df4e25bb7396517e6
Alvin Quach <alvinwquach@gmail.com>
2022-11-01 16:24:14 -0700

    Add nav and footer to blog page

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -3,6 +3,9 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
 
+import Navbar from "../components/global/Navbar";
+import Footer from "../components/global/Footer";
+
 import client from "../apollo-client";
 import {
   AllBlogCategoriesDocument,
@@ -51,64 +54,70 @@ export default function Blogs({ blogs, categories }: BlogsProps) {
   }, [activeCategory, blogs]);
 
   return (
-    <section className="bg-slate-150">
-      <div className="flex pb-12 flex-col items-center justify-center">
-        <h2 className="my-5 font-bold uppercase tracking-wide text-3xl bg-gradient-to-r from-blue-350 via-green-350 to-blue-550 bg-clip-text fill-transparent [-webkit-text-fill-color:transparent]">
-          Our Blog
-        </h2>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 mt-3 mb-20">
-        <div className="col-span-8 lg:col-span-2">
-          <Link href="/blogs">
-            <button className="block leading-5 text-blue-350 text-base no-underline font-bold tracking-wide hover:bg-accent-1 hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8 mb-4">
-              All Categories
-            </button>
-          </Link>
-          {categories?.map((category) => (
-            <Link
-              key={category.slug?.current}
-              href={`/blogs?category=${category?.slug?.current}`}
-            >
-              <button className="block text-sm leading-5 text-blue-550 hover:bg-accent-1 hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8 mb-2">
-                {category.name}
+    <>
+      <Navbar />
+      <section className="max-w-7xl mx-auto mt-5 px-5 sm:px-6 lg:px-8 py-20 sm:py-24 lg:py-24 bg-slate-150">
+        <div className="flex pb-12 flex-col items-center justify-center">
+          <h2 className="my-5 font-bold uppercase tracking-wide text-4xl bg-gradient-to-r from-blue-350 via-green-350 to-blue-550 bg-clip-text fill-transparent [-webkit-text-fill-color:transparent]">
+            Our Blog
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 mt-3 mb-20">
+          <div className="col-span-8 lg:col-span-2">
+            <Link href="/blogs">
+              <button className="block leading-5 text-blue-350 text-base no-underline font-bold tracking-wide hover:bg-accent-1 hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8 mb-4">
+                All Categories
               </button>
             </Link>
-          ))}
-        </div>
+            {categories?.map((category) => (
+              <Link
+                key={category.slug?.current}
+                href={`/blogs?category=${category?.slug?.current}`}
+              >
+                <button className="block text-sm leading-5 text-blue-550 hover:bg-accent-1 hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8 mb-2">
+                  {category.name}
+                </button>
+              </Link>
+            ))}
+          </div>
 
-        <div className="col-span-12 lg:col-span-10">
-          {filteredBlogs.length > 0 ? (
-            <div className="columns-1 gap-x-4 md:columns-2 lg:columns-3">
-              {filteredBlogs.map((blog) => {
-                return (
-                  <Link
-                    key={blog.slug?.current}
-                    href={`/blog/${blog.slug?.current}`}
-                  >
-                    <div className="mb-10 relative cursor-pointer rounded-sm overflow-hidden">
-                      <div className="h-[250px] relative">
-                        {blog.image?.asset?.url && (
-                          <Image
-                            src={blog.image.asset.url}
-                            alt={`Image for ${blog.title}`}
-                            fill
-                            className="object-cover"
-                          />
-                        )}
+          <div className="col-span-12 lg:col-span-10">
+            {filteredBlogs.length > 0 ? (
+              <div className="columns-1 gap-x-4 md:columns-2 lg:columns-3">
+                {filteredBlogs.map((blog) => {
+                  return (
+                    <Link
+                      key={blog.slug?.current}
+                      href={`/blog/${blog.slug?.current}`}
+                    >
+                      <div className="mb-10 relative cursor-pointer rounded-sm overflow-hidden">
+                        <div className="h-[250px] relative">
+                          {blog.image?.asset?.url && (
+                            <Image
+                              src={blog.image.asset.url}
+                              alt={`Image for ${blog.title}`}
+                              fill
+                              className="object-cover"
+                            />
+                          )}
+                        </div>
+                        <div className="bg-blue-350 py-3 px-3">
+                          <div className="text-white font-bold">
+                            {blog.title}
+                          </div>
+                        </div>
                       </div>
-                      <div className="bg-blue-350 py-3 px-3">
-                        <div className="text-white font-bold">{blog.title}</div>
-                      </div>
-                    </div>
-                  </Link>
-                );
-              })}
-            </div>
-          ) : (
-            <div className="text-sm">No blogs found.</div>
-          )}
+                    </Link>
+                  );
+                })}
+              </div>
+            ) : (
+              <div className="text-sm">No blogs found.</div>
+            )}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Footer />
+    </>
   );
 }
