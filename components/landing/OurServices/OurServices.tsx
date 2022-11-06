5825caf24ce2fdf9199d5793fce8b8a6da8470d6
Alvin Quach <alvinwquach@gmail.com>
2022-11-06 03:27:08 -0800

    Service cards route to page of service

--- components/landing/OurServices/OurServices.tsx
+++ components/landing/OurServices/OurServices.tsx
@@ -2,30 +2,34 @@ import React from "react";
 import { useState } from "react";
 import Image from "next/image";
 import cn from "clsx";
+import Link from "next/link";
 
 interface ServiceCardProps {
   imageUrl: string;
   name: string;
   description: string;
+  href: string;
 }
 
-function ServiceCard({ imageUrl, name, description }: ServiceCardProps) {
+function ServiceCard({ imageUrl, name, description, href }: ServiceCardProps) {
   return (
     <>
       <figure className="group inline-flex p-6 mb-4 w-full relative flex-col-reverse bg-white highlight-white/5 rounded-md shadow-md shadow-slate-300">
         <figcaption className="flex items-center space-x-4">
-          <Image
-            src={imageUrl}
-            alt={`Image of ${name}`}
-            className="flex-none rounded-2xl object-cover drop-shadow-md"
-            loading="lazy"
-            width={200}
-            height={200}
-          />
-          <div className="flex-auto">
-            <div className="text-base text-black font-semibold">{name}</div>
-            <div className="mt-0.5 text-black text-xs">{description}</div>
-          </div>
+          <Link href={href}>
+            <Image
+              src={imageUrl}
+              alt={`Image of ${name}`}
+              className="flex-none rounded-2xl object-cover drop-shadow-md"
+              loading="lazy"
+              width={200}
+              height={200}
+            />
+            <div className="flex-auto">
+              <div className="text-base text-black font-semibold">{name}</div>
+              <div className="mt-0.5 text-black text-xs">{description}</div>
+            </div>
+          </Link>
         </figcaption>
       </figure>
     </>
@@ -58,36 +62,43 @@ function OurServices({ hasShowMore = false }: ServicesProps) {
           >
             <div className="columns-1 md:columns-2 lg:colums-3">
               <ServiceCard
+                href="/service/physiotherapy"
                 imageUrl="/images/landing/services/physiotherapy.jpeg"
                 name="Physiotherapy"
                 description="Hands on Physiotherapy and Pelvic health that provide assessments and treatments of all injuries as well as everyday aches and pains."
               />
               <ServiceCard
+                href="/service/pelvic-health-physiotherapy"
                 imageUrl="/images/landing/services/pelvicHealthPhysiotherapy.jpeg"
                 name="Pelvic Health Physiotherapy"
                 description="Mindful Movement for Pelvic Floor/abdominal/core muscle."
               />
               <ServiceCard
+                href="/service/massage-therapy"
                 imageUrl="/images/landing/services/massageTherapy.jpeg"
                 name="Massage Therapy"
                 description="The massage therapy provider at Hands on Physiotherapy are regulated healthcare professionals and registered members of the College of Massage Therapists of Ontario."
               />
               <ServiceCard
+                href="/service/thai-massage"
                 imageUrl="/images/landing/services/thaiMassage.jpeg"
                 name="Thai Massage"
                 description="Thai massage is a traditional therapy combining acupressure, Indian Ayurvedic principles, and assisted yoga postures."
               />
               <ServiceCard
+                href="/service/chiropractor"
                 imageUrl="/images/landing/services/chiropractor.jpeg"
                 name="Chiropractor"
                 description="Chiropractic is a form of alternative medicine concerned with the diagnosis, treatment and prevention of mechanical disorders of the musculoskeletal system."
               />
               <ServiceCard
+                href="/service/hot-stone-physiotherapy"
                 imageUrl="/images/landing/services/hotStonePhysiotherapy.jpeg"
                 name="Hot Stone Physiotherapy"
                 description="An ancient technique to relieve pain and improve flexibility."
               />
               <ServiceCard
+                href="/service/acupuncture"
                 imageUrl="/images/landing/services/acupuncture.jpeg"
                 name="Acupuncture"
                 description="Acupuncture is a Form of Alternative medicine."
