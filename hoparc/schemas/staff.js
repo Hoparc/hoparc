1b6f00ed1edc557c38c22a1a095b1fc3c807e0c4
Sean Sipus <sean.sipus@gmail.com>
2022-11-07 19:16:45 -0500

    Create graphql schema

---
+++ hoparc/schemas/staff.js
@@ -0,0 +1,36 @@
+export default {
+  name: "staff",
+  title: "Staff",
+  type: "document",
+  fields: [
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      description: "Please upload an image of your staff member.",
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.
+      required(),
+    },
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      description: "Please provide the name of the staff member.",
+      validation: (Rule) => Rule.required(),
+    },
+      {
+        name: "description",
+        title: "Description",
+        type: "text",
+        description:
+          "Please provide a short description of the staff member.",
+          validation: (Rule) => [
+            Rule.required(),
+            Rule.max(50).warning("Short descriptions are usually better!"),
+          ],
+      },
+  ]
+}
