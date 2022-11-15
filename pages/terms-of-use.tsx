a24fc0efd361303afc0b48a4ff8b5a5a2196f2ff
Sean Sipus <sean.sipus@gmail.com>
2022-11-15 01:23:28 -0500

    Fix styling on information pages

--- pages/terms-of-use.tsx
+++ pages/terms-of-use.tsx
@@ -59,7 +59,7 @@ const TermsOfUse: NextPage = () => {
         <meta name="viewport" content="width=device-width" />
       </Head>
       <section
-        className="max-w-7xl mx-auto mt-5 px-5 sm:px-6 lg:px-8 py-20 sm:py-24 lg:py-24"
+        className="max-w-7xl mx-auto mt-5 px-5 sm:px-6 lg:px-8 py-20 sm:py-24 lg:py-24 font-roboto text-gray-850 dark:text-white"
         id="terms-of-use"
       >
         <h1 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
